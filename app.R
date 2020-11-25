e7f5fd0b44b3066087679fc4355fd8692e3fc5b3
Ben Garski <blgarski@uwm.edu>
2020-11-24 22:03:19 -0600

    ui

--- app.R
+++ app.R
@@ -39,12 +39,12 @@ ui <- navbarPage("DELPHI",
                           ),
                           fluidRow(
                             column(6,
-                                    withSpinner(plotlyOutput("designPlotly1"), type = 7, color = "#003087", size = 2),
-                                    withSpinner(plotlyOutput("designPlotly2"), type = 7, color = "#003087", size = 2)
+                                    fixedPanel(withSpinner(plotlyOutput("designPlotly1"), type = 7, color = "#003087", size = 2)),
+                                    fixedPanel(withSpinner(plotlyOutput("designPlotly2"), type = 7, color = "#003087", size = 2))
                             ),
                             column(6,
-                                    withSpinner(plotlyOutput("designPlotly3"), type = 7, color = "#003087", size = 2),
-                                    withSpinner(plotlyOutput("designPlotly4"), type = 7, color = "#003087", size = 2)
+                                    fixedPanel(withSpinner(plotlyOutput("designPlotly3"), type = 7, color = "#003087", size = 2)),
+                                    fixedPanel(withSpinner(plotlyOutput("designPlotly4"), type = 7, color = "#003087", size = 2))
                             )
                           )
                  ),
