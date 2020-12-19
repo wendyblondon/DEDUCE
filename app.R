5ff29395fa372368999d897562def8135ad732b0
Ben Garski <blgarski@uwm.edu>
2020-12-18 22:33:24 -0600

    Added input checks and changed loaders

--- app.R
+++ app.R
@@ -1,9 +1,10 @@
 library(shiny)
+library(shinydashboard)
 library(shinyBS)
 library(shinyjs)
 library(shinyWidgets)
-library(shinydashboard)
-library(shinycssloaders)
+library(shinyFeedback)
+library(waiter)
 library(tidyverse)
 
 source("target_crm.R")
@@ -54,7 +55,8 @@ ui <- dashboardPage(title = "DELPHI", skin = "black",
                       )
                     ),
                     dashboardBody(
-                      useShinyjs(), inlineCSS(CSS),
+                      useShinyjs(), inlineCSS(CSS), useShinyFeedback(), use_waiter(),
+                      waiter_show_on_load(logo = "logo.PNG", color = "white"),
                       tabItems(
                         tabItem(tabName = "Home",
                                 h1("Something Here")
@@ -62,14 +64,16 @@ ui <- dashboardPage(title = "DELPHI", skin = "black",
                         tabItem(tabName = "Design",
                                 fluidRow(
                                   column(3, style="overflow-y:scroll; height: 70vh;",
-                                         h3("Dose Escalation Designs:"),
+                                         h1("Inputs", style="text-align: center; text-decoration: underline;"),
+                                         br(),
                                          prettyCheckbox("DTSelectorTPT", "3+3", value = TRUE, status = "success", shape = "round", fill = TRUE, inline = TRUE),
                                          prettyCheckbox("DTSelectorTCRM", "TARGET-CRM", value = FALSE, status = "success", shape = "round", fill = TRUE, inline = TRUE),
                                          prettyCheckbox("DTSelectorOther", "Other", value = FALSE, status = "success", shape = "round", fill = TRUE, inline = TRUE),
-                                         textInput("DTDoseLabels", "Dose Level Labels", value = "-1,1,2,3"),
+                                         sliderInput("DTNumDoses", "How Many Doses Will There Be?", min = 3, max = 10, value = 4, width = "100%"),
+                                         textInput("DTDoseLabels", "Dose Level Labels", value = "-1,1,2,3", width = "100%"),
                                          bsTooltip("DTDoseLabels", "Please enter the dose level labels (seperated by commas) for each dose level evaluated in the trial", 
                                                    "top", options = list(container = "body")),
-                                         selectInput("DTStartLevel", "Starting Dose Level", choices = c(-1,1,2,3), selected = 1),
+                                         selectInput("DTStartLevel", "Starting Dose Level", choices = c(-1,1,2,3), selected = 1, width = "100%"),
                                          bsTooltip("DTStartLevel", "Please enter the starting dose level from the dose level labels above", 
                                                    "top", options = list(container = "body")),
                                          uiOutput("DTInputs"),
@@ -102,7 +106,7 @@ ui <- dashboardPage(title = "DELPHI", skin = "black",
 )
 
 server <- function(input, output, session) {
-  
+  waiter_hide()
   # Get Selected Designs for Rendering UI Guidance
   DTSelectedDesigns <- reactive({
     
@@ -134,22 +138,22 @@ server <- function(input, output, session) {
     # 3+3
     if (DTSelectedDesigns() == "some") {
       div(id="DTUISome",
-          sliderInput("DTTargetTox", "Target Toxicity Probability", min = 0, max = 1, value = 0.2, step = 0.1),
+          sliderInput("DTTargetTox", "Target Toxicity Probability", min = 0, max = 1, value = 0.2, step = 0.1, width = "100%"),
           bsTooltip("DTTargetTox", "Please enter the target toxicity probability of the study agent", 
                     "top", options = list(container = "body")),
-          sliderInput("DTNumTrials", "Number of Simulated Trials", min = 0, max = 10000, value = 100),
+          sliderInput("DTNumTrials", "Number of Simulated Trials", min = 0, max = 10000, value = 100, width = "100%"),
           bsTooltip("DTNumTrials", "Please enter the number of simulated trials. A larger number of simulations increases the precision of simulation results and computation time", 
                     "top", options = list(container = "body")),
-          textInput("DTTrueTox", "True Toxicity Probability Vector", value = "0.05,0.12,0.2,0.3"),
+          textInput("DTTrueTox", "True Toxicity Probability Vector", value = "0.05,0.12,0.2,0.3", width = "100%"),
           bsTooltip("DTTrueTox", "Please enter the true toxicity probabilities for each dose level evaluated in the trial. Toxicity probabilities must increase with each subsequent dose level", 
                     "top", options = list(container = "body")),
-          sliderInput("DTArrivalRate", "Patient Enrollment Rate", min = 0, max = 180, value = 15),
+          sliderInput("DTArrivalRate", "Patient Enrollment Rate", min = 0, max = 180, value = 15, width = "100%"),
           bsTooltip("DTArrivalRate", "Please enter the average time between enrolling patients (in days)", 
                     "top", options = list(container = "body")),
-          sliderInput("DTPropB", "Proportion of Patients from Cohort B", min = 0, max = 1, value = 0.1, step = 0.1),
+          sliderInput("DTPropB", "Proportion of Patients from Cohort B", min = 0, max = 1, value = 0.1, step = 0.1, width = "100%"),
           bsTooltip("DTPropB", "Please enter the proportion of enrolled patients belonging to the “enrichment” Cohort B", 
                     "top", options = list(container = "body")),
-          sliderInput("DTCycleLength", "Duration of DLT Observation Period", min = 0, max = 365, value = 28),
+          sliderInput("DTCycleLength", "Duration of DLT Observation Period", min = 0, max = 365, value = 28, width = "100%"),
           bsTooltip("DTCycleLength", "Please enter the duration of the DLT observation period (in days)", 
                     "top", options = list(container = "body"))
           
@@ -158,37 +162,37 @@ server <- function(input, output, session) {
     # TARGET-CRM or Both
     else if (DTSelectedDesigns() == "all") {
       div(id="DTUIAll",
-          textInput("DTPriorTox", "Prior Toxicity Probability Vector", value = "0.05,0.12,0.2,0.3"),
+          textInput("DTPriorTox", "Prior Toxicity Probability Vector", value = "0.05,0.12,0.2,0.3", width = "100%"),
           bsTooltip("DTPriorTox", "Please enter the prior toxicity probabilities for each dose level evaluated in the trial. Toxicity probabilities must increase with each subsequent dose level", 
                     "top", options = list(container = "body")),
-          sliderInput("DTTargetTox2", "Target Toxicity Probability", min = 0, max = 1, value = 0.2, step = 0.1),
+          sliderInput("DTTargetTox2", "Target Toxicity Probability", min = 0, max = 1, value = 0.2, step = 0.1, width = "100%"),
           bsTooltip("DTTargetTox2", "Please enter the target toxicity probability of the study agent", 
                     "top", options = list(container = "body")),
-          sliderInput("DTNumTrials2", "Number of Simulated Trials", min = 0, max = 10000, value = 100),
+          sliderInput("DTNumTrials2", "Number of Simulated Trials", min = 0, max = 10000, value = 100, width = "100%"),
           bsTooltip("DTNumTrials2", "Please enter the number of simulated trials. A larger number of simulations increases the precision of simulation results and computation time", 
                     "top", options = list(container = "body")),
-          textInput("DTTrueTox2", "True Toxicity Probability Vector", value = "0.05,0.12,0.2,0.3"),
+          textInput("DTTrueTox2", "True Toxicity Probability Vector", value = "0.05,0.12,0.2,0.3", width = "100%"),
           bsTooltip("DTTrueTox2", "Please enter the true toxicity probabilities for each dose level evaluated in the trial. Toxicity probabilities must increase with each subsequent dose level", 
                     "top", options = list(container = "body")),
-          sliderInput("DTArrivalRate2", "Patient Enrollment Rate", min = 0, max = 180, value = 15),
+          sliderInput("DTArrivalRate2", "Patient Enrollment Rate", min = 0, max = 180, value = 15, width = "100%"),
           bsTooltip("DTArrivalRate2", "Please enter the average time between enrolling patients (in days)", 
                     "top", options = list(container = "body")),
-          sliderInput("DTPropB2", "Proportion of Patients from Cohort B", min = 0, max = 1, value = 0.1, step = 0.1),
+          sliderInput("DTPropB2", "Proportion of Patients from Cohort B", min = 0, max = 1, value = 0.1, step = 0.1, width = "100%"),
           bsTooltip("DTPropB2", "Please enter the proportion of enrolled patients belonging to the “enrichment” Cohort B", 
                     "top", options = list(container = "body")),
-          selectInput("DTTargetCRM", "Target-CRM Option", choices = c(0,1,2), selected = 1),
+          selectInput("DTTargetCRM", "Target-CRM Option", choices = c(0,1,2), selected = 1, width = "100%"),
           bsTooltip("DTTargetCRM", "Please enter the desired variation of the TARGET-CRM design", 
                     "top", options = list(container = "body")),
-          sliderInput("DTMaxN", "Maximum Sample Size", min = 1, max = 200, value = 18),
+          sliderInput("DTMaxN", "Maximum Sample Size", min = 1, max = 200, value = 18, width = "100%"),
           bsTooltip("DTMaxN", "Please enter the maximum number of patients to be enrolled per trial", 
                     "top", options = list(container = "body")),
-          sliderInput("DTMinCohortB", "Minimum Enrollment of Cohort B Patients (Optional)", min = 0, max = 100, value = 2),
+          sliderInput("DTMinCohortB", "Minimum Enrollment of Cohort B Patients (Optional)", min = 0, max = 100, value = 2, width = "100%"),
           bsTooltip("DTMinCohortB", "Please enter the minimum number of Cohort B patients to be enrolled in the trial", 
                     "top", options = list(container = "body")),
-          sliderInput("DTCycleLength2", "Duration of DLT Observation Period", min = 0, max = 365, value = 28),
+          sliderInput("DTCycleLength2", "Duration of DLT Observation Period", min = 0, max = 365, value = 28, width = "100%"),
           bsTooltip("DTCycleLength2", "Please enter the duration of the DLT observation period (in days)", 
                     "top", options = list(container = "body")),
-          selectInput("DTCohortSize", "Cohort Size", choices = c(seq(1,9)), selected = 3),
+          selectInput("DTCohortSize", "Cohort Size", choices = c(seq(1,9)), selected = 3, width = "100%"),
           bsTooltip("DTCohortSize", "Please enter the cohort size. The cohort size is the number of patients to be treated at the current dose level before a dose escalation decision is made", 
                     "top", options = list(container = "body"))
       )
@@ -205,18 +209,56 @@ server <- function(input, output, session) {
     updateSliderInput(session, "DTMinCohortB", max = input$DTMaxN)
   })
   
+  ## Warning for Invalid Inputs
+  # Dose Labels
+  observeEvent(list(input$DTDoseLabels, input$DTNumDoses), {
+    req(input$DTDoseLabels)
+    feedbackDanger(
+      inputId = "DTDoseLabels",
+      show = length(unlist(strsplit(input$DTDoseLabels, ",")))!= input$DTNumDoses,
+      text = "The length must match the # of doses chosen above"
+    )
+  })
+  # True Tox
+  observeEvent(list(input$DTTrueTox, input$DTNumDoses), {
+    req(input$DTTrueTox)
+    feedbackDanger(
+      inputId = "DTTrueTox",
+      show = length(unlist(strsplit(input$DTTrueTox, ",")))!= input$DTNumDoses,
+      text = "The length must match the # of doses chosen above"
+    )
+  })
+  # True Tox 2
+  observeEvent(list(input$DTTrueTox2, input$DTNumDoses), {
+    req(input$DTTrueTox2)
+    feedbackDanger(
+      inputId = "DTTrueTox2",
+      show = length(unlist(strsplit(input$DTTrueTox2, ",")))!= input$DTNumDoses,
+      text = "The length must match the # of doses chosen above"
+    )
+  })
+  # Prior Tox
+  observeEvent(list(input$DTPriorTox, input$DTNumDoses), {
+    req(input$DTPriorTox)
+    feedbackDanger(
+      inputId = "DTPriorTox",
+      show = length(unlist(strsplit(input$DTPriorTox, ",")))!= input$DTNumDoses,
+      text = "The length must match the # of doses chosen above"
+    )
+  })
+  
   # Main Plotting UI
   output$DTPlotsUI <- renderUI({
     req(DTSelectedDesignsLength() > 0)
     hidden(
       div(id="DTUIPlots",
           column(6,
-                 withSpinner(plotOutput("DTPlot1", width = "100%", height = "100%"), type = 7, color = "#003087", size = 2),
-                 withSpinner(plotOutput("DTPlot2", width = "100%", height = "100%"), type = 7, color = "#003087", size = 2)
+                 plotOutput("DTPlot1", width = "100%", height = "100%"),
+                 plotOutput("DTPlot2", width = "100%", height = "100%"),
           ),
           column(6,
-                 withSpinner(plotOutput("DTPlot3", width = "100%", height = "100%"), type = 7, color = "#003087", size = 2),
-                 withSpinner(plotOutput("DTPlot4", width = "100%", height = "100%"), type = 7, color = "#003087", size = 2)
+                 plotOutput("DTPlot3", width = "100%", height = "100%"),
+                 plotOutput("DTPlot4", width = "100%", height = "100%"),
           )
       )
     )
@@ -251,6 +293,8 @@ server <- function(input, output, session) {
   
   # Running the Design(s)
   DTFunctionOutputs <- eventReactive(input$DTSimulate, {
+    w <- Waiter$new(html = spin_heartbeat(), color = "black")
+    w$show()
     
     # 3+3
     if (input$DTSelectorTPT == TRUE) {
@@ -282,8 +326,9 @@ server <- function(input, output, session) {
     }
     
     all <- list(get0("TPT"), get0("TCRM"), get0("Other"))
-    
+    w$hide()
     return(all[lengths(all) != 0])
+    
     
   })
   
