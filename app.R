e3f90b322e7bc317698af13213c594bbb32eda6c
Ben Garski <blgarski@uwm.edu>
2021-04-26 20:01:02 -0500

    verfied onclick works with using a different tab

--- app.R
+++ app.R
@@ -117,7 +117,7 @@ ui <- dashboardPage(title = "DEDUCE", skin = "black",
                       tabItems(
                         tabItem(tabName = "Home",
                                 column(12, style="overflow-y:scroll; height: 85vh;",
-                                  img(id="homeimg", src = "home.png"),
+                                  img(id="homeimg", src = "home.png", style="cursor: pointer;"),
                                   p(id="buttons",
                                     actionButton('jumpToDesign', "Design", tabName = "Design", class = "btn-primary btn-lg", id="btn-design"),
                                     actionButton("jumpToConduct", "Conduct", tabName = "Design", class = "btn-primary btn-lg", id="btn-conduct")
@@ -256,7 +256,7 @@ ui <- dashboardPage(title = "DEDUCE", skin = "black",
 server <- function(input, output, session) {
   
   # When Home Image Clicked Go To Home
-  onclick("homeimg", updateTabItems(session, inputId = "tabs", selected = "Home"))
+  onclick("homeimg", updateTabItems(session, inputId = "tabs", selected = "Design"))
   
   # Disable Results Button Until Design(s) Ran
   disable("DTResults")
