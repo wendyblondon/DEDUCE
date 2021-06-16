00fd17e2f80e43a2455eadcb7e515370d9f4918a
Ben Garski <blgarski@uwm.edu>
2021-06-16 14:54:34 -0500

    changed trial design and conduct to h4

--- app.R
+++ app.R
@@ -130,12 +130,12 @@ ui <- dashboardPage(title = "DEDUCE", skin = "black",
                                 h2(id="homeh4", "DEDUCE Modules:"),
                                 tags$ul(
                                   tags$li(
-                                    h3(class="home", "Trial Design"),
+                                    h4(class="home", "Trial Design"),
                                     p("Users can specify and compare the operating characteristics for hypothetical phase 1 designs through trial simulations, 
                                     and select an optimal design for the needs of the trial.")
                                   ),
                                   tags$li(
-                                    h3(class="home", "Trial Conduct"),
+                                    h4(class="home", "Trial Conduct"),
                                     p("Users can implement the adaptive trial, and determine the recommended dose level each time a new patient enrolls.")
                                   )    
                                 ),
