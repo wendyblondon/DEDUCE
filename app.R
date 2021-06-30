6b1f3e874f4e943354eefd10ef120a25477e567b
Ben Garski <blgarski@uwm.edu>
2021-06-30 15:42:26 -0500

    removed functions from app.R and sources funs.R

--- app.R
+++ app.R
@@ -11,96 +11,10 @@ library(rmarkdown)
 source("crm.R")
 source("target_crm.R")
 source("three_plus_three.R")
+source("funs.R")
 
 theme_set(theme_minimal(base_size = 15))
 
-# Functions
-numerizer <- function(x){
-  as.numeric(unlist(strsplit(x, ",")))
-}
-
-sequencer <- function(x){
-  seq(1,length(unlist(strsplit(x, ","))))
-}
-
-incrementCheck <- function(x) {
-  if (is.character(x)) {
-    
-    lengthX <- length(unlist(strsplit(x, ",")))
-    
-    if (lengthX > 1) {
-      vecX <- unlist(strsplit(x, ","))
-      
-      for (i in 1:(lengthX - 1)) {
-        if (vecX[i + 1] > vecX[i]) {
-          next
-        }
-        else{
-          return(FALSE)
-        }
-      }
-      return(TRUE)
-    }
-    else{
-      return(FALSE)
-    }
-  }
-  else{
-    stop("The input must be a character")
-  }
-}
-
-decimalCheck <- function(x){
-  if (is.character(x)) {
-    
-    lengthX <- length(unlist(strsplit(x, ",")))
-    
-    if (lengthX > 1) {
-      vecX <- unlist(strsplit(x, ","))
-      
-      for (i in 1:lengthX) {
-        if (grepl("[.]", vecX[i]) == FALSE) {
-          return(FALSE)
-        }
-        else{
-          next
-        }
-      }
-      return(TRUE)
-    }
-    else{
-      return(FALSE)
-    }
-  }
-  else{
-    stop("The input must be a character")
-  }
-}
-
-nullToNA <- function(x){
-  ifelse(is.null(x), NA, x)
-}
-
-designInputs <- function(x){
-  
-  trues <- which(x==TRUE)
-  
-  for (i in trues) {
-    if (i == 1) {
-      x[i] <-"3+3"
-    }
-    else if (i == 2) {
-      x[i] <- "TARGET-CRM"
-    }
-    
-    else{
-      x[i] <- "CRM"
-    }
-  }
-  
-  x <- x[trues]
-  return(x)
-}
 
 ui <- dashboardPage(title = "DEDUCE", skin = "black",
                     dashboardHeader(title = strong("DEDUCE")),
