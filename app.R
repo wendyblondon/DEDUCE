ba46013b82f662847f91f81b3198b10eafc9a792
Ben Garski <ben.garski@outlook.com>
2021-10-10 20:23:47 -0400

    added missing letter in logo

--- app.R
+++ app.R
@@ -78,7 +78,7 @@ ui <- dashboardPage(title = "DEDUCE", skin = "black",
                                 fluidRow(
                                   column(12, align="center",
                                          a(href="https://www.danafarberbostonchildrens.org", img(id="df_logo", src = "danafarber_bostonchildrens_logo.png", style="cursor: pointer;"), target="_blank", rel="noopener noreferrer"),
-                                         a(href="https://www.NorthwesternMutual.com", img(id="nm_logo", src = "NLogo.png", style="cursor: pointer;"), target="_blank", rel="noopener noreferrer"),
+                                         a(href="https://www.NorthwesternMutual.com", img(id="nm_logo", src = "NMLogo.png", style="cursor: pointer;"), target="_blank", rel="noopener noreferrer"),
                                          a(href="https://hms.harvard.edu/", img(id="hms_logo", src = "HMS.png", style="cursor: pointer;"), target="_blank", rel="noopener noreferrer")
                                   )
                                 )
